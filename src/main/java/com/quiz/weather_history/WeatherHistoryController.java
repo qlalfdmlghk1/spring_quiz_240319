859ba36a0f3b6a46c11a183d6e7134feea2ebf2c
DESKTOP-5P9OAHR\user <test@test.com>
2024-06-25 18:51:24 +0900

    06.25 수업

--- src/main/java/com/quiz/weather_history/WeatherHistoryController.java
+++ src/main/java/com/quiz/weather_history/WeatherHistoryController.java
@@ -1,26 +1,47 @@
 package com.quiz.weather_history;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.List;
 
-@RequestMapping("/lesson05")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.quiz.weather_history.bo.WeatherHistoryBO;
+import com.quiz.weather_history.domain.WeatherHistory;
+
+@RequestMapping("/weather-history")
 @Controller
 public class WeatherHistoryController {
 	
-	@GetMapping("/quiz04")
-	public String quiz04() {
+	@Autowired
+	private WeatherHistoryBO weatherHistoryBO;
+
+	// 날씨 목록 화면 /weather-history/weather-list-view
+	@GetMapping("/weather-list-view")
+	public String weatherListView(Model model) {
+		// select db
+		List<WeatherHistory> weatherHistoryList = weatherHistoryBO.getWeatherHistoryList();
+		
+		// model 담기
+		model.addAttribute("weatherHistoryList",weatherHistoryList);
+		
+		// 화면 이동
 		return "weather_history/weatherList";
 	}
 	
-	@RequestMapping(path="/add-weather-view", method = RequestMethod.GET)
+	// 날씨 추가 화면 /weather-history/add-weather-view
+	@GetMapping("/add-weather-view")
 	public String addWeatherView() {
 		return "weather_history/addWeather";
 	}
 	
-	@RequestMapping(path="/weather-list-view", method = RequestMethod.GET)
-	public String weatherListView() {
-		return "weather_history/weatherList";
+	// 날씨를 추가 기능  /weather-history/add-weather
+	@PostMapping("/add-weather")
+	public String addWeather(
+			@RequestParam("date") 타입 ) {
+		
 	}
 }
---
+++ src/main/java/com/quiz/weather_history/mapper/WeatherHistoryMapper.java
@@ -0,0 +1,12 @@
+package com.quiz.weather_history.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.quiz.weather_history.domain.WeatherHistory;
+
+@Mapper
+public interface WeatherHistoryMapper {
+	public List<WeatherHistory> selectWeatherHistoryList();
+}
