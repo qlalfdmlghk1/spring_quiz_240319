0bc3830cd0e1433532eabcfd22f2b6ec0ab78171
CHOI <cws98tiger@naver.com>
2024-06-25 13:18:49 +0900

    06.24 과제

--- src/main/java/com/quiz/weather_history/WeatherHistoryController.java
+++ src/main/java/com/quiz/weather_history/WeatherHistoryController.java
@@ -1,5 +1,26 @@
 package com.quiz.weather_history;
 
-public class WeatherHistoryController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@RequestMapping("/lesson05")
+@Controller
+public class WeatherHistoryController {
+	
+	@GetMapping("/quiz04")
+	public String quiz04() {
+		return "weather_history/weatherList";
+	}
+	
+	@RequestMapping(path="/add-weather-view", method = RequestMethod.GET)
+	public String addWeatherView() {
+		return "weather_history/addWeather";
+	}
+	
+	@RequestMapping(path="/weather-list-view", method = RequestMethod.GET)
+	public String weatherListView() {
+		return "weather_history/weatherList";
+	}
 }
